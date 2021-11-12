97c7c5703b99dd04b6aecc9a79643d1a86caf0bb
Your Name <you@example.com>
2021-11-12 20:27:13 +0100

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,11 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    `gatsby-plugin-postcss`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
+        tailwindConfig: `tailwind.config.js`,
         name: "images",
         path: "./src/images/",
       },
---
+++ src/components/header.js
@@ -0,0 +1,75 @@
+import { graphql, useStaticQuery, Link } from "gatsby";
+import React, { useState } from "react";
+
+
+function Header() {
+  const [isExpanded, toggleExpansion] = useState(false);
+  const { site } = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <header className="bg-purple-400">
+      
+      <div className="flex flex-wrap items-center justify-between max-w-4xl p-4 mx-auto md:p-8">
+        <Link to="/">
+          <h1 className="flex items-center text-2xl font-bold text-white no-underline">
+          Belissimo
+           
+          </h1>
+        </Link>
+
+        <button
+          className="items-center block px-3 py-2 text-white border border-white rounded md:hidden"
+          onClick={() => toggleExpansion(!isExpanded)}
+        >
+         
+        </button>
+
+        <nav
+        
+          className={`${
+            isExpanded ? `block` : `hidden`
+          } md:block md:items-center w-full md:w-auto`}
+        >
+          {[
+            {
+              route: `/home`,
+              title: `Home`,
+            },
+            {
+              route: `/about`,
+              title: `About`,
+            },
+            {
+              route: `/menu`,
+              title: `Menu`,
+            },
+            {
+              route: `/contact`,
+              title: `Contact`,
+            },
+          ].map((link) => (
+            <Link
+              className="block mt-4 text-white no-underline md:inline-block md:mt-0 md:ml-6"
+              key={link.title}
+              to={link.route}
+            >
+              {link.title}
+            </Link>
+          ))}
+         
+        </nav>
+      </div>
+     
+    </header>
+  );
+}
+
+export default Header;
